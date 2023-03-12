9eb28cf3a24c076bb52d92a6e32a457572a30b25
JotaADev <jonasalmeidareis@gmail.com>
2023-03-12 12:19:07 -0300

    Design CardGame

--- src/components/CardGame/CardGame.js
+++ src/components/CardGame/CardGame.js
@@ -4,10 +4,18 @@ import './CardGame.css'
 const CardGame = () => {
   return (
     <>
-      <div className='CardGame'>
-        <div className='CardGameImg'>Imagem</div>
-        <div className='CardGameInfos'>Infos</div>
-      </div>
+      <a className='CardGameLink' href='/'>
+        <div className='CardGame'>
+          <div>
+            <img className='CardGameImg' src='https://www.elamigos-games.com/storage/games_tumbl/forza-horizon-4-ultimate-edition-cover-2wz.jpg' width='100%' height='100%'></img>
+          </div>
+          <div className='CardGameInfos'>
+            <h1>Forza Horizon 4 Ultimate Edition</h1>
+            <p>76.7Gb</p>
+            <button>Download</button>
+          </div>
+        </div>
+      </a>
     </>
   )
 }
