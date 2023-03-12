2b1c849d709c7edcd1c67a91f54e8826bc3ae910
JotaADev <jonasalmeidareis@gmail.com>
2023-03-12 17:14:40 -0300

    Inclusão dos jogos

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
+import React from "react";
 import CardGame from "./components/CardGame/CardGame";
 import Header from "./components/Header/Header";
 
 function App() {
+  const [games, setGames] = React.useState([]);
+
+  async function fetchGames() {
+    const response = await fetch('./games.json')
+    const json = await response.json()
+    setGames(json)
+  }
+
+  React.useEffect(() => {fetchGames()}, [])
+
   return (
     <>
       <Header/>
       <section className="AreaGames">
-        <CardGame/>
-        <CardGame/>
-        <CardGame/>
-        <CardGame/>
-        <CardGame/>
-        <CardGame/>
-        <CardGame/>
-        <CardGame/>
-        <CardGame/>
+        {games.map((game) => (
+          <CardGame img={game.gameimg}
+          nomejogo={game.nomejogo}
+          tamanho={game.tamanhojogo}
+          linkdownload={game.linkdownload}/>
+        ))}
       </section>
     </>
   );
--- src/components/CardGame/CardGame.js
+++ src/components/CardGame/CardGame.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import './CardGame.css'
 
-const CardGame = () => {
+const CardGame = ({img, nomejogo, tamanho, linkdownload}) => {
+  function btnDownload() {
+    window.open(linkdownload)
+  }
+
   return (
     <>
       <a className='CardGameLink' href='/'>
         <div className='CardGame'>
           <div>
-            <img className='CardGameImg' src='https://www.elamigos-games.com/storage/games_tumbl/forza-horizon-4-ultimate-edition-cover-2wz.jpg' width='100%' height='100%'></img>
+            <img className='CardGameImg' src={img} width='100%' height='100%'></img>
           </div>
           <div className='CardGameInfos'>
-            <h1>Forza Horizon 4 Ultimate Edition</h1>
-            <p>76.7Gb</p>
-            <button>Download</button>
+            <h1>{nomejogo}</h1>
+            <p>{tamanho}Gb</p>
+            <button onClick={btnDownload}>Download</button>
           </div>
         </div>
       </a>
