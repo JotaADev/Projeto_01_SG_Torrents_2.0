073e497c18e4aede266a4bfa55589aa2a2829748
JotaADev <jonasalmeidareis@gmail.com>
2023-03-13 21:34:28 -0300

    Final Version

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,11 +2,56 @@ import React from 'react'
 import './SearchBar.css'
 
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = React.useState('');
+  const [searchResults, setSearchResults] = React.useState([]);
+  const [games, setGames] = React.useState([]);
+
+  async function carregarGames() {
+    const resposta = await fetch('./games.json');
+    const gamesJSON = await resposta.json();
+    setGames(gamesJSON);
+  }
+
+  React.useEffect(() => {
+    carregarGames()
+  }, [])
+
+  const handleSearch = (event) => {
+    const results = games.filter((game) =>
+    game.nomejogo.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setSearchResults(results);
+  };
+
+  const ResultSearch = () => {
+    return(
+      <div className='SearchBarResult'>
+        {searchResults.map((game) => (
+          <a key={game.id} href='/'>
+            <div>
+              <h4>{game.nomejogo}</h4>
+            </div>
+          </a>
+        ))}
+      </div>
+    )
+  }
+
   return (
     <>
-        <input className='SearchBarInput' placeholder='Qual jogo está procurando?'></input>
+      <div className='SearchBarBox'>
+        <input
+          type="text"
+          className='SearchBarInput'
+          placeholder='Qual jogo está procurando?'
+          value={searchTerm}
+          onChange={(event) => setSearchTerm(event.target.value)}
+          onKeyUp={handleSearch}
+        />
+        {searchTerm != '' ? <ResultSearch/> : null}
+      </div>
     </>
-  )
+  );
 }
 
 export default SearchBar
